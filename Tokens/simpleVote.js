5bf24154889412938dc8eff6bf5e1d788324e9b9
kwon hyunwoo <khw6428@gmail.com>
2019-06-30 18:58:57 +0900

    Modify HTML and JS file for Token Switching

--- Tokens/simpleVote.js
+++ Tokens/simpleVote.js
@@ -1,65 +1,639 @@
-let contractAddress = '0x6aD780a8eb0C34e54C1B18B329A0Ec85EAB6Ca76';
-let abi = 
- [
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "adopters",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0x43ae80d3"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getAdopters",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address[16]"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0x3de4eb17"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "petId",
-        "type": "uint256"
-      }
-    ],
-    "name": "adopt",
-    "outputs": [
-      {
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function",
-    "signature": "0x8588b2c5"
-  }
+let controllerAddr = '0x97c2454754b167a24879b0453388f6fdaba7a4b0';
+
+let controllerAbi = 
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "convertTokens",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewBakingMasteryTokenAddr",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewButterTokenAddr",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
-let simpleVoteContract;
-let simpleVote;
+let butterTokenAddr = '0xe8d38fD0Ed900890A6A2387B6cCBafFe72eAa3A3';
+let butterAbi =
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "buyToken",
+		"outputs": [
+			{
+				"name": "boughtTokens",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "value",
+				"type": "uint256"
+			},
+			{
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "convertLTKtoGTK",
+		"outputs": [
+			{
+				"name": "result",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "sendETH",
+		"outputs": [
+			{
+				"name": "result",
+				"type": "bool"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_symbol",
+				"type": "string"
+			},
+			{
+				"name": "minter",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "who",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getETH",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewcontroller",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+let bakingMasteryAddr = '0xA1Dbf760e1F3fD057199dBAE78A0a4d20c981E77'; 
+let bakingMasteryAbi =
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "convertLTKtoGTK",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "createAccount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "getLTK",
+		"outputs": [
+			{
+				"name": "remains",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_symbol",
+				"type": "string"
+			},
+			{
+				"name": "minter",
+				"type": "address"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "who",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewcontroller",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+let tokenControllerContract;
+let tokenController;
+let butterContract;
+let butterToken;
+let bakingMasteryContract;
+let bakingMastery;
 let accountAddress;
 let currentEtherBalance;
 let currentTokenBalance;
@@ -82,9 +656,17 @@ window.addEventListener('load', function() {
 });
 
 function startApp() {
-  simpleVoteContract = web3.eth.contract(abi);
-  simpleVote = simpleVoteContract.at(contractAddress);
-  document.getElementById('contractAddr').innerHTML = getLink(contractAddress);
+  tokenControllerContract = web3.eth.contract(controllerAbi); // controller
+  tokenController = tokenControllerContract.at(controllerAddr);
+  document.getElementById('controllerAddr').innerHTML = getLink(controllerAddr);
+
+  butterContract = web3.eth.contract(butterAbi); // controller
+  butterToken = tokenControllerContract.at(butterTokenAddr);
+  document.getElementById('butterAddr').innerHTML = getLink(butterTokenAddr);
+
+  bakingMasteryContract = web3.eth.contract(bakingMasteryAbi); // controller
+  bakingMastery = tokenControllerContract.at(bakingMasteryAddr);
+  document.getElementById('bakingMasteryAddr').innerHTML = getLink(bakingMasteryAddr);
 
   web3.eth.getAccounts(function(e,r){
   document.getElementById('accountAddr').innerHTML = getLink(r[0]);
@@ -94,7 +676,7 @@ function startApp() {
 }
 
 function getLink(addr) {
-  return '<a target="_blank" href=https://ropsten.etherscan.io/tx/' + addr + '>' + addr +'</a>';
+  return '<a target="_blank" href=https://rinkeby.etherscan.io/address/' + addr + '>' + addr +'</a>';
 }
 
 function getValue() {
@@ -111,84 +693,57 @@ function getEther() {
 }
 
 function getToken() {
-  simpleVote.getTokenBought(function(e,r){
-    document.getElementById('tokenValue').innerHTML = r.toString();
+  butterToken.getBalanceOf(function(e,r){
+    document.getElementById('butterTokenValue').innerHTML = r.toString();
+  });
+
+  bakingMastery.getBalanceOf(function(e,r){
+    document.getElementById('bakingTokenValue').innerHTML = r.toString();
   });
 }
 
 function getTokenInfo() {
 
-  simpleVote.getTotalToken(function(e,r){
-    document.getElementById('tokens-total').innerHTML = r.toString();
+  butterToken.getInitialSupply(function(e,r){
+    document.getElementById('butter-tokens-total').innerHTML = r.toString();
   });
-  simpleVote.getBalanceTokens(function(e,r){
-    document.getElementById('tokens-sellable').innerHTML = r.toString();
+  butterToken.getTotalSupply(function(e,r){
+    document.getElementById('butter-tokens-sellable').innerHTML = r.toString();
   });
-  simpleVote.getTokenPrice(function(e,r){
+  butterToken.getTokenPrice(function(e,r){
     tokenPrice = parseFloat(web3.fromWei(r.toString()));
-    document.getElementById('token-cost').innerHTML = tokenPrice + "ETH";
+    document.getElementById('butter-token-cost').innerHTML = tokenPrice + "ETH";
   });
-  web3.eth.getBalance(simpleVote.address, function(e,v) {
-    document.getElementById('contract-balance').innerHTML = web3.fromWei(v.toString()) + "ETH";
+  web3.eth.getBalance(butterToken.address, function(e,v) {
+    document.getElementById('butter-contract-balance').innerHTML = web3.fromWei(v.toString()) + "ETH";
   });
-}
 
-function getCandidateInfo() {
-  simpleVote.getVotesReceivedFor(function(e,r){
-    for(let i=1;i<=r.length;i++)
-    {
-      document.getElementById('day_votes_' + i).innerHTML = r[i-1].toString();
-    }
+  bakingMastery.totalSupply(function(e,r){
+    document.getElementById('baking-tokens-total').innerHTML = r.toString();
+  });
+  bakingMastery.totalSupply(function(e,r){
+    document.getElementById('baking-tokens-sellable').innerHTML = r.toString();
   });
 }
 
-function voteForCandidate(param) {
-  //let candidateName = $("#candidate").val();
-	let candidateName = "";
-	let voteTokens = 1;
-	switch(param)
-	{
-		case 1 :
-			candidateName = "Monday";
-			break; 
-		case 2 :
-			candidateName = "Tuesday";
-			break;
-		case 3 :
-			candidateName = "Wednesday";
-			break;
-		case 4 :
-			candidateName = "Thursday";
-			break;
-		case 5 :
-			candidateName = "Friday";
-			break;
-		case 6 :
-			candidateName = "Saturday";
-			break;
-		case 7 :
-			candidateName = "Sunday";
-			break;
-	}
-
-  $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
-
-	simpleVote.adopt(param,{from: web3.eth.accounts[0]},function(e,r){
-		console.log("not error");
-	});
-  // simpleVote.vote(candidateName, voteTokens, function (e, r){
-  //   getCandidateInfo();
-  // });
-}
 
 function buyTokens() {
   let tokensToBuy = $("#buy").val();
   let price = tokensToBuy * tokenPrice;
   $("#buy-msg").html("Purchase order has been submitted. Please wait.");
 
-  simpleVote.buy({value: web3.toWei(price, 'ether'), from: web3.eth.accounts[0]}, function(v) {
-    web3.eth.getBalance(simpleVote.address, function(e, r) {
+  butterToken.buyToken({value: web3.toWei(price, 'ether'), from: web3.eth.accounts[0]}, function(v) {
+    web3.eth.getBalance(butterToken.address, function(e, r) {
     $("#contract-balance").html(web3.fromWei(r.toString()) + " ETH");
    });
   });
+}
+
+
+function switchTokens() {
+  let tokensToSwitch = $("#switch").val();
+  let price = tokensToSwitch;
+  $("#switch-msg").html("Switch order has been submitted. Please wait.");
+
+  tokenController.convertTokens({value: price, from: web3.eth.accounts[0]});
 }
