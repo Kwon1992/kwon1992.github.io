aec19e20f02f9397de492f9070a89d7d45828512
kwon hyunwoo <khw6428@gmail.com>
2019-06-30 20:27:47 +0900

    Edit Controller codes

--- Tokens/TokenController.sol
+++ Tokens/TokenController.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.5.2;
 
-import "./ButterToken.sol";
-import "./BakingMastery.sol";
+import "https://github.com/Kwon1992/kwon1992.github.io/blob/master/Tokens/ButterToken.sol";
+import "https://github.com/Kwon1992/kwon1992.github.io/blob/master/Tokens/BakingMastery.sol";
 
 contract TokenController{
     address owner;
@@ -9,16 +9,24 @@ contract TokenController{
 
     constructor() public {
         owner = msg.sender;
-        tokens[0] = new ButterToken("ButterToken", "BTK", msg.sender);
-        //tokens[1] = new LeaderToken();
+        tokens[0] = address(new ButterToken("ButterToken", "BTK", msg.sender));
+        tokens[1] = address(new BakingMastery("BakingMasteryToken","BMTK", msg.sender));
     }
 
-    function convertTokens(uint256 amount) returns (uint256) { // BakingMastery to Butter
+    function convertTokens(uint256 amount) public returns (uint256) { // BakingMastery to Butter
         //여기서의 msg.sender == Token 교환을 요청한 user
         require(BakingMastery(tokens[1]).convertLTKtoGTK(amount, msg.sender));
         ButterToken(tokens[0]).convertLTKtoGTK(amount/100, msg.sender);
         return amount/100;
     } 
+    
+    function viewButterTokenAddr() external view returns (address) {
+        return tokens[0];
+    }
+    
+    function viewBakingMasteryTokenAddr() external view returns (address) {
+        return tokens[1];
+    }
 
 }
 
