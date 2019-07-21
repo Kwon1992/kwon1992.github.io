1e591c014483c294e6a37a7b05956830b3444ae1
kwon hyunwoo <khw6428@gmail.com>
2019-07-22 02:26:26 +0900

    update testing folder
    update testing folder

--- testing/minesweeper/js/main.js
+++ testing/minesweeper/js/main.js
@@ -16,6 +16,7 @@
 
 /*----- constants -----*/
 
+
 console.log(sessionStorage);
 
 var bombImage = '<img src="images/bomb.png">';
@@ -59,6 +60,8 @@ const predict = document.getElementById("item_predict");
 
 var board;
 var bombCount;
+var bombCoordList = "";
+var SHA256_value;
 
 // bomb var
 var winner;
@@ -259,6 +262,15 @@ boardEl.addEventListener('touchend', function(e){
 });
 // 아이템??
 
+document.getElementById('result-btn').addEventListener('click', function() {
+  sessionStorage.clear();
+  window.location.href='index.html';
+});
+
+
+console.log(document.getElementById('copy-btn'));
+
+
 
 //제거 예정 리스너#2
 function createResetListener() { 
@@ -291,13 +303,22 @@ function revealAll() { // 모든 셀 열기
 };
 
 
-function unveilAll() { // 모든 셀 열기 (아이템 사용 시)
-  console.log("UNVEIL!!!");
+function unveil() { // 모든 셀 열기 (아이템 사용 시)
+
+  
+  var revealNum = Math.floor(sizeLookup[sessionStorage.getItem("level")].size * rowSize / 3);
+  var curVeilNum = 0;
+
   var tdList = Array.from(document.querySelectorAll('[data-row]')); // [data-row]라는 attribute를 가진 모든 DOM을 array로!!
-  tdList.forEach(function(td) {
+
+  tdList.some(function(td) {
+
+
     var rowIdx = parseInt(td.getAttribute('data-row'));
     var colIdx = parseInt(td.getAttribute('data-col'));
     var cell = board[rowIdx][colIdx]; 
+    
+    curVeilNum += 1;
 
     if (cell.isBomb) { // 폭탄이라면
         td.innerHTML = bombImage; // 폭탄 이미지
@@ -306,13 +327,14 @@ function unveilAll() { // 모든 셀 열기 (아이템 사용 시)
         td.textContent = cell.adjBombs; // 글자 담음
     } else { // 아무것도 없는 빈 칸이라면
     }
+
+    return curVeilNum == revealNum;
   });
 };
 
-function veilAll() { // 모든 셀 열기 (아이템 사용 시)
-  console.log("VEIL!!!");
+function veil() { // 모든 셀 열기 (아이템 사용 시)
   var tdList = Array.from(document.querySelectorAll('[data-row]')); // [data-row]라는 attribute를 가진 모든 DOM을 array로!!
-  tdList.forEach(function(td) {
+  tdList.some(function(td) {
     td.innerHTML = "";
     td.textContent = "";
   });
@@ -415,21 +437,8 @@ function buildCells(){ // core Function #3
   });
 
 
-  addBombs(); // 모든 2차원배열을 cell로 만든 뒤 폭탄을 추가함. 
-  // addBombs() 내부에서 인접 폭탄 개수 계산하도록 변경... (updating)
-  
-
-  /*
-  runCodeForAllCells(function(cell){ // 맨 하단에 runCodeForAllCells에 관한 함수 존재.
-    cell.calcAdjBombs();
-  }); 
-  // 폭탄 추가 후 각 cell마다 자신의 인접 셀에 폭탄 잇는지 확인 후 폭탄 개수 처리
-  */
-};
-// forEach 함수 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-// 인자#1 currentValue :처리할 현재 요소 & 인자#2 index:처리할 현재 요소의 인덱스.
-
-
+  addBombs(); // 모든 2차원배열을 cell로 만든 뒤 폭탄을 추가함.
+}
 
 
 function getBombCount() {
@@ -453,9 +462,8 @@ function getBombCount() {
 
 
 
-function addBombs() { // 랜덤하게 폭탄 설치 core Function #4 _ updating...
+function  addBombs() { // 랜덤하게 폭탄 설치 core Function #4 _ updating...
   var currentTotalBombs = sizeLookup[level].totalBombs;
-  var bombCoordList = ""; //string으로 사용할 예정.
   while (currentTotalBombs !== 0) {
     var row = Math.floor(Math.random() * rowSize);
     var col = Math.floor(Math.random() * colSize);
@@ -473,9 +481,9 @@ function addBombs() { // 랜덤하게 폭탄 설치 core Function #4 _ updating.
       // 인접 cell에 폭탄 갯수 추가
     }
   }
-  console.log(bombCoordList); // 실제 위치를 확인해보기 위한 log 출력
-  return bombCoordList; // 나중에 hash값으로 변환하기 위해 string값 return
-};
+
+   // 나중에 hash값으로 변환하기 위해 string값 return
+}
 /* "//"부분은 임의로 추가할 부분 - update 예정
 */
 
@@ -496,8 +504,8 @@ function useItem() {
   
 
   if(sessionStorage.getItem("revealAll") === "true"){
-      setTimeout(unveilAll,0);
-      setTimeout(veilAll,2000);
+      setTimeout(unveil,0);
+      setTimeout(veil,2000);
   }
 }
 
@@ -519,16 +527,6 @@ function getWinner() {
   return true;
 }
 
-
-
-
-
-
-
-
-
-
-
 function init() { // map 제작 + 초기 변수 설정
   buildTable();
   board = buildArrays();
@@ -547,13 +545,6 @@ function init() { // map 제작 + 초기 변수 설정
 
 
 
-
-
-
-
-
-
-
 function render() {
   checkItem();
   //아이템 선택여부 체크
@@ -562,9 +553,6 @@ function render() {
   var seconds = timeElapsed % 60; // 어따 써? 
   var tdList = Array.from(document.querySelectorAll('[data-row]')); // [data-row]라는 attribute를 가진 모든 DOM을 array로!!
   
-
-
-
   tdList.forEach(function(td) {
     var rowIdx = parseInt(td.getAttribute('data-row'));
     var colIdx = parseInt(td.getAttribute('data-col'));
@@ -601,9 +589,13 @@ function render() {
         td.innerHTML = wrongBombImage; // 잘못된 폭탄 이미지 넣음
       }
     });
+
+    popUp();
+
   } else if (winner) { // winner라면
     document.getElementById('reset').innerHTML = '<img src=images/cool-face.png>';
     clearInterval(timerId); // interval 종료
+    popUp();
   }
 
   
@@ -618,7 +610,142 @@ function runCodeForAllCells(cb) {
   });
 }
 
-//  실제 실행함수 2
-init();
-render(); // bug 지점
 
+function popUp(){
+  document.getElementById('bomb-coord').innerHTML = bombCoordList;
+  document.getElementById('hashValue').innerHTML = "<b>SHA256: </b>" + SHA256(bombCoordList);
+  $('#myModal').delay(1500).show(0);
+}
+// function close_pop(flag) { // eventlistener
+//   $('#myModal').hide();
+// };
+
+
+
+init();
+render(); 
+
+
+
+
+
+
+//-------------------------------------------------------------------------------
+
+/**
+*
+*  Secure Hash Algorithm (SHA256)
+*  http://www.webtoolkit.info/
+*
+*  Original code by Angel Marin, Paul Johnston.
+*
+**/
+function SHA256(s){
+    var chrsz   = 8;
+    var hexcase = 0;
+  
+    function safe_add (x, y) {
+        var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+        var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+        return (msw << 16) | (lsw & 0xFFFF);
+    }
+  
+    function S (X, n) { return ( X >>> n ) | (X << (32 - n)); }
+    function R (X, n) { return ( X >>> n ); }
+    function Ch(x, y, z) { return ((x & y) ^ ((~x) & z)); }
+    function Maj(x, y, z) { return ((x & y) ^ (x & z) ^ (y & z)); }
+    function Sigma0256(x) { return (S(x, 2) ^ S(x, 13) ^ S(x, 22)); }
+    function Sigma1256(x) { return (S(x, 6) ^ S(x, 11) ^ S(x, 25)); }
+    function Gamma0256(x) { return (S(x, 7) ^ S(x, 18) ^ R(x, 3)); }
+    function Gamma1256(x) { return (S(x, 17) ^ S(x, 19) ^ R(x, 10)); }
+  
+    function core_sha256 (m, l) {
+        var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
+        var HASH = new Array(0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19);
+        var W = new Array(64);
+        var a, b, c, d, e, f, g, h, i, j;
+        var T1, T2;
+        m[l >> 5] |= 0x80 << (24 - l % 32);
+        m[((l + 64 >> 9) << 4) + 15] = l;
+        for ( var i = 0; i<m.length; i+=16 ) {
+            a = HASH[0];
+            b = HASH[1];
+            c = HASH[2];
+            d = HASH[3];
+            e = HASH[4];
+            f = HASH[5];
+            g = HASH[6];
+            h = HASH[7];
+            for ( var j = 0; j<64; j++) {
+                if (j < 16) W[j] = m[j + i];
+                else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
+                T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
+                T2 = safe_add(Sigma0256(a), Maj(a, b, c));
+                h = g;
+                g = f;
+                f = e;
+                e = safe_add(d, T1);
+                d = c;
+                c = b;
+                b = a;
+                a = safe_add(T1, T2);
+            }
+            HASH[0] = safe_add(a, HASH[0]);
+            HASH[1] = safe_add(b, HASH[1]);
+            HASH[2] = safe_add(c, HASH[2]);
+            HASH[3] = safe_add(d, HASH[3]);
+            HASH[4] = safe_add(e, HASH[4]);
+            HASH[5] = safe_add(f, HASH[5]);
+            HASH[6] = safe_add(g, HASH[6]);
+            HASH[7] = safe_add(h, HASH[7]);
+        }
+        return HASH;
+    }
+    function str2binb (str) {
+        var bin = Array();
+        var mask = (1 << chrsz) - 1;
+        for(var i = 0; i < str.length * chrsz; i += chrsz) {
+            bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i%32);
+        }
+        return bin;
+    }
+    function Utf8Encode(string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    }
+    function binb2hex (binarray) {
+        var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+        var str = "";
+        for(var i = 0; i < binarray.length * 4; i++) {
+            str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
+            hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
+        }
+        return str;
+    }
+    s = Utf8Encode(s);
+    return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
